763d8d9a465d4bb9441ead20738c251179caa268
osman <osman@DESKTOP-5UNKHH2>
2021-01-31 17:02:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200;
+            double dolarDun = 1.44;
+            double dolarBugun = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+
+            if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else 
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+
+        }
+    }
+}
